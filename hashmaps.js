4a007e471d5553c42bb93eaa79000d668578ee11
ThomasChun <tchun2055@gmail.com>
2018-12-17 11:30:42 -1000

    completed palindrome

--- hashmaps.js
+++ hashmaps.js
@@ -49,9 +49,14 @@ class HashMap{
     if(Math.floor(this.size / 2) <= this.items){
       array = this.resizeHashMap();
     }
-    const key = Object.keys(data);
+    let key;
+    if (typeof yourVariable === 'object') {
+      key = Object.keys(data);
+    } else {
+      key = data;
+    }
     let hashPosition = this.hashString(String(key)) % this.size;
-    while(array[hashPosition] !== Number.MIN_SAFE_INTEGER){
+    while(array[hashPosition] !== Number.MIN_SAFE_INTEGER && this.array[hashPosition] !== Number.MAX_SAFE_INTEGER){
       hashPosition = (hashPosition + 1) % this.size;
     }
     array[hashPosition] = data;
@@ -59,11 +64,13 @@ class HashMap{
   }
 
   print(){
+    let array = [];
     for(let i = 0; i < this.size; i++){
       if(this.array[i] !== Number.MIN_SAFE_INTEGER){
-        console.log(this.array[i]);
+        array.push(this.array[i]);
       }
     }
+    return array;
   }
 
   getItem(key) {
@@ -73,7 +80,7 @@ class HashMap{
       console.log('Element is not in the hashamp');
       return 0;
     } 
-    while (this.array[hashPosition] !== Number.MIN_SAFE_INTEGER) {
+    while (this.array[hashPosition] !== Number.MIN_SAFE_INTEGER && this.array[hashPosition] !== Number.MAX_SAFE_INTEGER) {
       let hashKey = Object.keys(this.array[hashPosition]);
       if(String(hashKey) === key) {
         array.push(this.array[hashPosition][hashKey]);
@@ -81,6 +88,39 @@ class HashMap{
       hashPosition = (hashPosition + 1) % this.size;
     }
     return array;
+  }
+
+  hasItem(key) {
+    let hashPosition = this.hashString(key) % this.size;
+    if (this.array[hashPosition] === Number.MIN_SAFE_INTEGER) {
+      return false;
+    } 
+    let hashKey;
+    while (this.array[hashPosition] !== Number.MIN_SAFE_INTEGER && this.array[hashPosition] !== Number.MAX_SAFE_INTEGER) {
+      typeof(this.array[hashPosition]) === 'object'  ? hashKey = Object.keys(this.array[hashPosition]) : hashKey = this.array[hashPosition];
+      if(String(hashKey) === key) {
+        return true;
+      }
+      hashPosition = (hashPosition + 1) % this.size;
+    }
+  }
+
+  delete(key) {
+    let hashPosition = this.hashString(key) % this.size;
+    if (this.array[hashPosition] === Number.MIN_SAFE_INTEGER) {
+      console.log('DELETE: Item does not exist');
+      return;
+    } 
+    let hashKey;
+    while (this.array[hashPosition] !== Number.MIN_SAFE_INTEGER && this.array[hashPosition] !== Number.MAX_SAFE_INTEGER) {
+      typeof(this.array[hashPosition]) === 'object'  ? hashKey = Object.keys(this.array[hashPosition]) : hashKey = this.array[hashPosition];
+      if(String(hashKey) === key) {
+        this.array[hashPosition] = Number.MAX_SAFE_INTEGER;
+        this.items--;
+        return;
+      }
+      hashPosition = (hashPosition + 1) % this.size;
+    }
   }
 }
 
@@ -92,4 +132,21 @@ array.forEach(function(element) {
   hashMap.insert(element, hashMap.array);
 });
 // console.log(hashMap);
-console.log('GET ITEM: ',hashMap.getItem('Maiar'));
+// console.log('GET ITEM: ',hashMap.getItem('Maiar'));
+// console.log(hashMap.print());
+
+
+
+
+function palindrome(sentence) {
+  const hashMap1 = new HashMap(26);
+  let key;
+  for (let i = 0; i < sentence.length; i++) {
+    key = sentence.charAt(i);
+    hashMap1.hasItem(key) ? hashMap1.delete(key) : hashMap1.insert(key, hashMap1.array);
+  }
+  return hashMap1.items > 1 ? false : true;
+}
+
+// console.log(palindrome('testing')); // false
+// console.log(palindrome('racecar')); // true
